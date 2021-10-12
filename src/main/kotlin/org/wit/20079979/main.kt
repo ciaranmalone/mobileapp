6c88a1e1aeec11c618dba531fb43872c9201f173
ciaranmalone <ciaranmalone27@gmail.com>
2021-10-12 11:03:56 +0100

    simple menu

--- src/main/kotlin/org/wit/20079979/main.kt
+++ src/main/kotlin/org/wit/20079979/main.kt
@@ -5,5 +5,30 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>){
-    println("trying")
+    logger.info { "Launching Game" }
+    println("SpinTop v0.1")
+
+    var input: Int
+
+    input = menu()
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Play!")
+    println(" 2. Play Again!")
+    println(" 3. List Scoreboard")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
